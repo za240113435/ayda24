7bc8929eb1ff87d7e8175d6e38762781131c1afb
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2024-09-20 21:43:38 -0600

    listo cifrado

--- alumnos/cifra-lista.py
+++ alumnos/cifra-lista.py
@@ -8,32 +8,73 @@
 Cifra la lista de alumnos
 """
 
+import base64
 import os
+from getpass import getpass
+from hashlib import sha256
+
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
-def cifra(cadena: str):
+def cifra(cadena: str, f: Fernet):
     """Función que cifra una cadena"""
-    return cadena.title()
+
+    token = f.encrypt(cadena.encode())
+    return token.decode()
 
 
-def cifra_archivo(archivo: str):
+def calcula_cifrado(password: str, sal: str):
+    """Función que calcula el algoritmo de cifrado"""
+
+    bpassword = password.encode()
+
+    # Obtenemos los primeros 16 bytes
+    # salt = os.urandom(16)
+    bsal = sha256(sal.encode()).digest()[:16]
+
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(),
+        length=32,
+        salt=bsal,
+        iterations=1_000_000,
+    )
+
+    key = base64.urlsafe_b64encode(kdf.derive(bpassword))
+
+    f = Fernet(key)
+
+    return f
+
+
+def cifra_archivo(archivo: str, password: str, sal: str):
     """Función que cifra un archivo"""
+
+    # Cadena usada para guardar el cifrado del archivo
+    cifrado = ""
+
+    # Algoritmos de Fernet
+    f = calcula_cifrado(password, sal)
 
     with open(archivo, encoding="utf-8") as csv_file:
         for line in csv_file:
-            line = line.strip()
             campos = line.split(",")
 
             if campos[0] == "No":
-                print(line)
+                cifrado = line
                 continue
 
-            campos[1] = cifra(campos[1])
-            campos[2] = cifra(campos[2])
-            campos[3] = cifra(campos[3])
+            campos[1] = cifra(campos[1], f)
+            campos[2] = cifra(campos[2], f)
+            # Quitamos el salto de linea con strip
+            campos[3] = cifra(campos[3].strip(), f)
 
             linea = ",".join(campos)
-            print(linea)
+            cifrado += linea + "\n"
+
+    with open("cifrado.csv", "w", encoding="utf-8") as csv_file:
+        csv_file.write(cifrado)
 
 
 def main():
@@ -46,10 +87,10 @@ def main():
         print(f"El archivo: {archivo}, no existe")
         return
 
-    # password = getpass("Ingresa password: ")
-    # sal = getpass("Ingresa la sal del algoritmo: ")
+    password = getpass("Ingresa password: ")
+    sal = getpass("Ingresa la sal del algoritmo: ")
 
-    cifra_archivo(archivo)
+    cifra_archivo(archivo, password, sal)
 
 
 if __name__ == "__main__":
