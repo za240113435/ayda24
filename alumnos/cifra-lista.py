6553db45c5437a09dab261d28553c53256e562f1
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2024-09-20 20:49:32 -0600

    cifra lista v0.1

--- alumnos/cifra-lista.py
+++ alumnos/cifra-lista.py
@@ -8,12 +8,48 @@
 Cifra la lista de alumnos
 """
 
+import os
+
+
+def cifra(cadena: str):
+    """Función que cifra una cadena"""
+    return cadena.title()
+
+
+def cifra_archivo(archivo: str):
+    """Función que cifra un archivo"""
+
+    with open(archivo, encoding="utf-8") as csv_file:
+        for line in csv_file:
+            line = line.strip()
+            campos = line.split(",")
+
+            if campos[0] == "No":
+                print(line)
+                continue
+
+            campos[1] = cifra(campos[1])
+            campos[2] = cifra(campos[2])
+            campos[3] = cifra(campos[3])
+
+            linea = ",".join(campos)
+            print(linea)
+
 
 def main():
     """
-    Comentario de la función
+    Función principal para cifrar un archivo csv
     """
-    print("Hola Mundo")
+    archivo = input("Ingresa el nombre el archivo CSV: ")
+
+    if not os.path.exists(archivo):
+        print(f"El archivo: {archivo}, no existe")
+        return
+
+    # password = getpass("Ingresa password: ")
+    # sal = getpass("Ingresa la sal del algoritmo: ")
+
+    cifra_archivo(archivo)
 
 
 if __name__ == "__main__":
