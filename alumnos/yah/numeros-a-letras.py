b62232ef8d219e5dd1987d691294bd534775e67c
Yolanda Alejo Huerta <za240113432@zapopan.tecmm.edu.mx>
2024-10-15 20:15:40 -0600

    hasta el 99

--- alumnos/yah/numeros-a-letras.py
+++ alumnos/yah/numeros-a-letras.py
@@ -3,9 +3,60 @@
 Programa que convierte de números a letras
 """
 
+PRIMEROS = (
+    "cero",
+    "uno",
+    "dos",
+    "tres",
+    "cuatro",
+    "cinco",
+    "seis",
+    "siete",
+    "ocho",
+    "nueve",
+    "diez",
+    "once",
+    "doce",
+    "trece",
+    "catorce",
+    "quince",
+)
+
+SEGUNDOS = (
+    "",
+    "dieci",
+    "veinti",
+    "treinta",
+    "cuarenta",
+    "cincuenta",
+    "sesenta",
+    "setenta",
+    "ochenta",
+    "noventa",
+)
+
 
 def numero_a_letra(numero):
-    pass
+    """Función que convierte el número a letras"""
+
+    en_letra = ""
+    tamanio = len(numero)
+
+    if tamanio == 1:
+        en_letra = PRIMEROS[int(numero)]
+    elif tamanio == 2:
+        if numero[0] == "1" and numero[1] < "6":
+            en_letra = PRIMEROS[int(numero)]
+        elif numero == "20":
+            en_letra = SEGUNDOS[int(numero[0])][:-1] + "e"
+        elif numero[1] == "0":
+            en_letra = SEGUNDOS[int(numero[0])]
+        elif numero < "31":
+            en_letra = SEGUNDOS[int(numero[0])] + PRIMEROS[int(numero[1])]
+        else:
+            en_letra = SEGUNDOS[int(numero[0])] + " y " + PRIMEROS[int(numero[1])]
+
+    return en_letra
 
 
 def ingresa_numero():
