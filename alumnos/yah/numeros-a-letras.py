eedb8670258847772a02897de71e3d760fbe018e
Yolanda Alejo Huerta <za240113432@zapopan.tecmm.edu.mx>
2024-10-15 19:39:01 -0600

    mejoramis ingresa número

--- alumnos/yah/numeros-a-letras.py
+++ alumnos/yah/numeros-a-letras.py
@@ -4,10 +4,18 @@ Programa que convierte de números a letras
 """
 
 
+def numero_a_letra(numero):
+    pass
+
+
 def ingresa_numero():
     """Función que devuelve un número en formato str"""
 
-    numero = input("Ingresa un número entero:")
+    numero = input("Ingresa un número entero positivo: ")
+
+    while not numero.isdecimal():
+        print("No es un número entero, por favor ", end="")
+        numero = input("ingresa un número entero positivo: ")
 
     return numero
 
@@ -18,8 +26,10 @@ def main():
     """
     numero = ingresa_numero()
 
-    if numero:
-        print("ok")
+    en_letra = numero_a_letra(numero)
+
+    if en_letra:
+        print("El número", numero, "es:", en_letra)
 
 
 if __name__ == "__main__":
