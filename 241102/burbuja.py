33c3fc5ac77fce043a34329d1873228802719cb4
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2024-11-02 07:50:15 -0600

    agregar burbuja

---
+++ 241102/burbuja.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# c-basic-offset: 4; tab-width: 8; indent-tabs-mode: nil
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+# :indentSize=4:tabSize=8:noTabs=true:
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import random
+
+CANTIDAD = 10
+
+
+def burbuja(lista):
+    return lista
+
+
+def es_valido(lista):
+    tamanio = len(lista)
+    esta_ordenado = False
+
+    for pos in range(tamanio - 1):
+        if lista[pos] >= lista[pos + 1]:
+            break
+    else:
+        esta_ordenado = True
+
+    return esta_ordenado
+
+
+def main():
+    desordenado = [random.randint(1, 100) for _ in range(CANTIDAD)]  # nosec B311
+    # Este si esta ordenado
+    # desordenado = [ x for x in range(CANTIDAD) ]
+
+    # Algoritmo de ordenamiento
+    ordenado = burbuja(desordenado)
+
+    if not es_valido(ordenado):
+        print("No esta ordenado")
+    else:
+        print("La lista SI esta ordenada")
+
+
+if __name__ == "__main__":
+    main()
