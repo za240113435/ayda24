d1b4a943b9762db0d95d6400fe6a3cf1d076fd97
Hugo Alberto Gonzalez Martinez <za240113445@zapopan.tecmm.edu.mx>
2024-11-02 08:35:39 -0600

    Burbuja HAGM

--- 241102/v2/burbuja.py
+++ 241102/v2/burbuja.py
@@ -9,37 +9,42 @@ import random
 
 CANTIDAD = 10
 
-
 def burbuja(lista):
+    tamanio = len(lista)
+    for i in range(tamanio - 1):
+        for j in range(tamanio - 1 - i):
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
     return lista
-
 
 def es_valido(lista):
     tamanio = len(lista)
     esta_ordenado = False
 
     for pos in range(tamanio - 1):
-        if lista[pos] >= lista[pos + 1]:
+        if lista[pos] > lista[pos + 1]:
             break
     else:
         esta_ordenado = True
 
     return esta_ordenado
 
-
 def main():
-    desordenado = [random.randint(1, 100) for _ in range(CANTIDAD)]  # nosec B311
-    # Este si esta ordenado
-    # desordenado = [ x for x in range(CANTIDAD) ]
-
-    # Algoritmo de ordenamiento
+    desordenado = [random.randint(1, 100) for _ in range(CANTIDAD)]
+    
+    # Lista desordenada
+    print("Lista desordenada:", desordenado)
+    
+    # Burbuja
     ordenado = burbuja(desordenado)
+    
+    # Lista ordenada
+    print("Lista ordenada:", ordenado)
 
     if not es_valido(ordenado):
         print("No esta ordenado")
     else:
         print("La lista SI esta ordenada")
-
 
 if __name__ == "__main__":
     main()
