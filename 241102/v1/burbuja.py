d55f80590aeb739b2ead86c29d3a159f62afa3c6
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2024-11-09 09:22:19 -0600

    analizando burbuja

--- 241102/v1/burbuja.py
+++ 241102/v1/burbuja.py
@@ -5,21 +5,23 @@
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import random
 import time
 
-CANTIDAD = 10
+CANTIDAD = 10_000
 
 
-def burbuja(lista):
-    tamanio = len(lista)
+def burbuja(lista):  # T(n) =  9n(n-1) + 2 ->  9n^2 - 9n + 2 => n^2
+    # T(10,000) = 3s -> 9(10,000)^2 + 9(10,000) + 2 ->  n^2
+    # T(100,000) =  T(10,000 x 10) -> 3s x 10^2 -> 3 x 100 -> 300s
+    tamanio = len(lista)  # 1
 
-    for i in range(tamanio):
-        for j in range(tamanio - i - 1):
-            if lista[j] > lista[j + 1]:
+    for i in range(tamanio):  # n -> 9n(n-1)
+        for j in range(tamanio - i - 1):  # n - i - 1  -> 9(n-1)
+            if lista[j] > lista[j + 1]:  # 3 + 6 = 9
                 # Intercambia los elementos
-                lista[j], lista[j + 1] = lista[j + 1], lista[j]
-    return lista
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]  # 6
+
+    return lista  # 1
 
 
 def es_valido(lista):
@@ -36,9 +38,15 @@ def es_valido(lista):
 
 
 def main():
-    desordenado = [random.randint(1, 100) for _ in range(CANTIDAD)]  # nosec B311
+    # Caso promedio
+    # desordenado = [random.randint(1, 100) for _ in range(CANTIDAD)]  # nosec B311
+    # Mejor caso
     # Este si esta ordenado
-    # desordenado = [ x for x in range(CANTIDAD) ]
+    # desordenado = [x for x in range(CANTIDAD)]  # nosec B311
+    # Peor caso
+    # Este esta ordenado a la inversa
+    # desordenado = [CANTIDAD-x for x in range(CANTIDAD)]
+    desordenado = [x for x in range(CANTIDAD, 0, -1)]
 
     tiempo_inicial = time.time()
     # Algoritmo de ordenamiento
