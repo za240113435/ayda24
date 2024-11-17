4c24e9bf48b428403c72e1b0553cbbf3cb4255bf
Jose Manuel Bautista <jose.bautista@tamazula.tecmmm.edu.mx>
2024-11-16 22:45:15 -0600

    Tarde pero sin sueño mi versión del Quicksort JMBV

--- 241102/v2/quicksort.py
+++ 241102/v2/quicksort.py
@@ -4,22 +4,34 @@
 # :indentSize=4:tabSize=8:noTabs=true:
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-
+#Jose Manuel Bautista
 import random
+import time
 
-CANTIDAD = 10
-
+CANTIDAD = 1000
 
 def quicksort(lista):
-    return lista
+    if len(lista) <= 1:  # Caso base: lista de 0 o 1 elemento ya está ordenada
+        return lista
+    else:
+        # Selecciona el pivote
+        pivote = lista[len(lista) // 2]
 
+        # Particiona la lista en tres partes
+        menores = [x for x in lista if x < pivote]
+        iguales = [x for x in lista if x == pivote]
+        mayores = [x for x in lista if x > pivote]
+
+        # Combina las partes ordenadas
+        return quicksort(menores) + iguales + quicksort(mayores)
 
 def es_valido(lista):
+    #print(lista)
     tamanio = len(lista)
-    esta_ordenado = False
+    esta_ordenado = True
 
     for pos in range(tamanio - 1):
-        if lista[pos] >= lista[pos + 1]:
+        if lista[pos] > lista[pos + 1]:
             break
     else:
         esta_ordenado = True
@@ -32,8 +44,14 @@ def main():
     # Este si esta ordenado
     # desordenado = [ x for x in range(CANTIDAD) ]
 
+    tiempo_inicial = time.time()
     # Algoritmo de ordenamiento
     ordenado = quicksort(desordenado)
+    tiempo_final = time.time()
+
+    tiempo_total = tiempo_final - tiempo_inicial
+
+    print("Se tardo un total de", tiempo_total, "segundos")
 
     if not es_valido(ordenado):
         print("No esta ordenado")
